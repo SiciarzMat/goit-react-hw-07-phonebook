9e4ebda63784e30aadabe99b4cbbf37cd19e7cf9
Mateusz Siciarz <siciarz.mat@gmail.com>
2023-05-07 18:13:19 +0200

    Task Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import { ContactForm } from "./ContactForm";
+import { ContactList } from "./ContactList";
+import { Filter } from "./Filter";
+import { Section } from "./Section";
+import { selectError, selectIsLoading } from "redux/selectors";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from "redux/operations";
+import styled from "styled-components";
+
+const StyledNotification = styled.b`
+display: inline-block;
+padding: 20px;`
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Phonebook">
+        <ContactForm />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        {isLoading && !error &&
+          <StyledNotification>
+            Request in progress...
+          </StyledNotification>}
+        <ContactList />
+      </Section>
+    </>
   );
 };
